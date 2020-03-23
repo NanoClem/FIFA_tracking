6ba9f4273d8df8f56c1e4a89147cc36f87c891fb
MaxencePRSZ <maxenceperso@gmail.com>
2020-03-23 16:56:30 +0100

    Test OCR

---
+++ fifa_app/ocr.py
@@ -0,0 +1,15 @@
+import cv2
+
+# Reading the video
+vidcap = cv2.VideoCapture('capture.mp4')
+success, image = vidcap.read()
+count = 0
+success = True
+idx = 0
+
+print(success)
+
+# Read the video frame by frame
+while success:
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    cv2.imshow("lelel", hsv)
