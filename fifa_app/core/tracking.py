cd71f050db8f5a03e96f655d533ae6f6b1562107
Evan Dadure <evan.dadure@gmail.com>
2020-05-26 17:59:04 +0200

    Update tracking.py

--- fifa_app/core/tracking.py
+++ fifa_app/core/tracking.py
@@ -9,7 +9,7 @@ post_team_API({'name': 'Marseille'})
 post_team_API({'name': 'Paris'})
 print("The teams has been sent.")
 
-# To send the useful frames of a video the API
+# To send the useful frames of a video to the API
 print("Starting to scan the video...")
 coords_field = [449, 415, 791, 621]  # x,y of the upper-left corner, x,y of the bottow-right corner)
 post_all_frames('capture.mp4', coords_field,450,2000,"Marseille","Paris")
