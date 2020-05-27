acb2b64c7fe678e799d76dfa15e5b08b4f0f0a9e
NanoClem <clemdecoop.pro@gmail.com>
2020-05-27 05:08:06 +0200

    add a slider to change heat value

--- web/public/assets/js/charts/heatmap.js
+++ web/public/assets/js/charts/heatmap.js
@@ -36,12 +36,32 @@ function createHeatmap(config, positions, width, height, max, val) {
   };
   
   heatmapInstance.setData(heatmapData);
+  return heatmapInstance;
 }
 
 
 /* ==================================================
-    ////////// CREATE HEATMAPS //////////
+    ////////// OPERATIONS ON HEATMAPS //////////
 ===================================================*/
+/**
+ * Update each heat value in given heatmap data
+ * @param {Array} data
+ * @param {Number} new_value 
+ */
+function updateHeatValues(data) {
+
+  let tmp = [];
+  data.forEach(heatData => {
+    tmp = heatData['data'];
+    // set new heat value
+    tmp['data'].forEach(point => {
+      point['value'] = heatData['new_value'];
+    });
+    // update data and repaint the heatmap
+    heatData['heatmap'].setData(tmp);
+  });
+}
+
 /**
  * 
  */
@@ -68,8 +88,30 @@ function initHeatMaps() {
     // get every position by team
     p = getPositionsPerTeam(data);
 
-    createHeatmap(config1, {pos: p['p1'], xMax: p['xMax'], yMax: p['yMax']}, w, h, max, val);
-    createHeatmap(config2, {pos: p['p2'], xMax: p['xMax'], yMax: p['yMax']}, w, h, max, val);
+    // heatmap instances
+    let h1 = createHeatmap(config1, {pos: p['p1'], xMax: p['xMax'], yMax: p['yMax']}, w, h, max, val);
+    let h2 = createHeatmap(config2, {pos: p['p2'], xMax: p['xMax'], yMax: p['yMax']}, w, h, max, val);
+
+
+    // listening to heat slider change
+    d3.select("#heatSlider").on("change", function (d) {
+
+      // new slide value
+      let newValue = this.value;
+
+      // heatmap data
+      let d1 = h1.getData();
+      let d2 = h2.getData();
+
+      let prepared = [
+        {"heatmap": h1, "data": d1, "new_value": newValue},
+        {"heatmap": h2, "data": d2, "new_value": newValue}
+      ];
+      
+      // update heat values in each dataset
+      updateHeatValues(prepared);
+    })
+    
   });
 }
 
