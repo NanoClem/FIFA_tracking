27589685f8517a8c972336342bbb7adc712a2f35
NanoClem <clemdecoop.pro@gmail.com>
2020-05-27 05:49:44 +0200

    smooth heat value change when sliding

--- web/public/assets/js/charts/heatmap.js
+++ web/public/assets/js/charts/heatmap.js
@@ -8,7 +8,7 @@
  * @param {*} val 
  */
 function createHeatmap(config, positions, width, height, max, val) {
-  
+
   var heatmapInstance = h337.create(config);
 
   // now generate some random data
@@ -36,6 +36,8 @@ function createHeatmap(config, positions, width, height, max, val) {
   };
   
   heatmapInstance.setData(heatmapData);
+
+  
   return heatmapInstance;
 }
 
@@ -43,6 +45,13 @@ function createHeatmap(config, positions, width, height, max, val) {
 /* ==================================================
     ////////// OPERATIONS ON HEATMAPS //////////
 ===================================================*/
+
+// DATA QUEUE
+d3.queue()
+    .defer(d3.json, "http://localhost:3000/api/frame?video_id=5ecdcc69521f95f867ba1eaa")
+    .awaitAll(initHeatMaps);
+
+
 /**
  * Update each heat value in given heatmap data
  * @param {Array} data
@@ -59,37 +68,41 @@ function updateHeatValues(data) {
     });
     // update data and repaint the heatmap
     heatData['heatmap'].setData(tmp);
-    heatData['heatmap'].repaint();
   });
 }
 
 
+/**
+ * 
+ * @param {*} heatmaps 
+ */
 function listenHeatChange(heatmaps) {
 
-  d3.select("#heatSlider").on("change", function (d) {
-    let newValue = this.value;
-    let preparedData = [];
-    let tmpData = [];
+  let newValue = document.getElementById("heatSlider").value;
+  let preparedData = [];
+  let tmpData = [];
 
-    heatmaps.forEach(h => {
-      tmpData = h.getData();
-      preparedData.push( {"heatmap": h, "data": tmpData, "new_value": newValue} );
-    });
-
-    updateHeatValues(preparedData);
+  heatmaps.forEach(h => {
+    tmpData = h.getData();
+    preparedData.push( {"heatmap": h, "data": tmpData, "new_value": newValue} );
   });
+
+  updateHeatValues(preparedData);
 }
 
 /**
  * 
  */
-function initHeatMaps() {
+function initHeatMaps(err, resData) {
+  if (err) throw err;
+
   // params
   var w = 525,
     h = 340,
     max = 100,
     val = 20,
     p = {};
+    data = resData[0];
 
   // configs
   var config1 = {
@@ -99,21 +112,26 @@ function initHeatMaps() {
     container: document.getElementById('heatmap2')
   };
 
-  // Import data
-  d3.json("assets/datasets/chart_dataset.json", function(err, data) {
-    if (err) throw err;
+  // get every position by team
+  p = getPositionsPerTeam(data);
 
-    // get every position by team
-    p = getPositionsPerTeam(data);
+  // heatmap instances
+  let h1 = createHeatmap(config1, {pos: p['p1'], xMax: p['xMax'], yMax: p['yMax']}, w, h, max, val);
+  let h2 = createHeatmap(config2, {pos: p['p2'], xMax: p['xMax'], yMax: p['yMax']}, w, h, max, val);
 
-    // heatmap instances
-    let h1 = createHeatmap(config1, {pos: p['p1'], xMax: p['xMax'], yMax: p['yMax']}, w, h, max, val);
-    let h2 = createHeatmap(config2, {pos: p['p2'], xMax: p['xMax'], yMax: p['yMax']}, w, h, max, val);
+  // creating heat slider
+  d3.select("#heatUpdater")
+    .append("input")
+      .attr("type", "range")
+      .attr("min", 0)
+      .attr("max", max)
+      .attr("step", "1")
+      .attr("value", val)
+      .attr("id", "heatSlider")
+      .on("input", function input() {
+        listenHeatChange([h1, h2]);
+      });
 
-    // listening to heat slider change
-    listenHeatChange([h1, h2]);
-  });
+  // listening to heat slider change
+  listenHeatChange();
 }
-
-
-initHeatMaps();
