31d2c14f9e0e76bbd3cad39529c7a61989ba3756
NanoClem <clemdecoop.pro@gmail.com>
2020-05-29 05:09:37 +0200

    add new field 'teams'

--- fifa_app/api/video/models.py
+++ fifa_app/api/video/models.py
@@ -7,5 +7,6 @@ def create_video_model(ns):
     return ns.model('Video', {
         '_id'  : fields.String(decription='video unique identifier'),
         'path' : fields.String(description='path of the video file'),
-        'title': fields.String(description='title of the video')
+        'title': fields.String(description='title of the video'),
+        'teams': fields.List(fields.String(), description='ids of teams involved in the match')
     })
