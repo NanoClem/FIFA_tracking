bfc505994c0b2a77c534fb3d88cc82580f3a60a3
NanoClem <clemdecoop.pro@gmail.com>
2020-05-29 08:34:28 +0200

    update in function of team name and color

--- web/public/assets/js/chartsController.js
+++ web/public/assets/js/chartsController.js
@@ -2,9 +2,7 @@
  * 
  * @param {*} data 
  */
-function updateCharts(data) {
-
-    let colors = ['#e41a1c','#377eb8']
+function updateCharts(data, colors) {
 
     // remove content of each chart
     d3.select("#vbarChartSVG").remove();
@@ -13,12 +11,12 @@ function updateCharts(data) {
     d3.select("#heatSlider").remove();
 
     // then update
-    if (data.length > 0) {
+    if (data.constructor == Array && data.length > 0) {
+        // find involved teams and filter duplicates
         updateVpresence(780, 520, data, colors);
         updateHpresence(780, 520, data, colors);
         initHeatMaps(data);
     }
-    
 }
 
 
@@ -26,8 +24,20 @@ function updateCharts(data) {
  * 
  * @param {*} data 
  */
-function upDateTeamNames(data) {
-    // console.log(data);
+function upDateTeamLabels(teamsData) {
+
+    if (teamsData.constructor == Array && teamsData.length > 0) {
+        // first team label
+        d3.select("#lc1")
+            .style("background-color", teamsData[0].jersey_color)
+            .select(".teamLabel")
+                .text(teamsData[0].name)
+        // second team label
+        d3.select("#lc2")
+        .style("background-color", teamsData[1].jersey_color)
+        .select(".teamLabel")
+            .text(teamsData[1].name)
+    }
 }
 
 
@@ -39,8 +49,15 @@ function upDateTeamNames(data) {
 function upDateAll(err, resData) {
 
     if (err) throw err;
-    upDateTeamNames(resData[1]);
-    updateCharts(resData[0]);
+
+    // get involved teams data
+    d3.json("http://localhost:3000/api/team/" + resData[1]['teams'][0] + "/" + resData[1]['teams'][1], function (err2, data) {
+        if (err2) throw err2;
+
+        let colors = data.map(function (d) { return d.jersey_color; });
+        upDateTeamLabels(data);
+        updateCharts(resData[0], colors);
+    });
 }
 
 
@@ -54,7 +71,7 @@ function notifyChange(DOMelement) {
     d3.queue()
         .defer(d3.json, "http://localhost:3000/api/frame/video/" + id)  // get all frame associated to this video
         .defer(d3.json, "http://localhost:3000/api/video/" + id)        // get data about the video
-        .awaitAll(upDateAll)
+        .awaitAll(upDateAll);
 }
 
 
